2fbf25167dd74d6b3b1f220f2b09c5e1a62edf12
StrategySam <thestrategysam@gmail.com>
2024-02-04 10:20:48 -0600

    2 more buttons

---
+++ src/main/java/frc/robot/commands/ButtonCommands/Safe.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.ButtonCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+public class Safe extends Command {
+
+    private Arm arm;
+    private Intake intake;
+
+    public Safe(Arm arm, Intake intake){
+        this.arm = arm;
+        addRequirements(arm);
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize(){
+    }
+
+    @Override
+    public void execute(){
+        arm.setAngle(10);
+        intake.stopTake();
+    }
+
+    @Override
+    public void end(boolean interupt){
+        
+    }
+    
+    @Override
+	public boolean isFinished() {
+		return true;
+	}
+}
+
