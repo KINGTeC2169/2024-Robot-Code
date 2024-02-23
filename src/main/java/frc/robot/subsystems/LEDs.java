b1ddda8477a8b7b1dfa4a5609fe2416eaa5ac8b1
Rishi Hari <rishihari4608@gmail.com>
2024-02-23 16:15:41 -0600

    added comments to leds

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -5,23 +5,23 @@ import frc.robot.Constants;
 
 public class LEDs {   
     private static SerialPort arduino = new SerialPort(9600, Constants.Ports.arduino);
-
+//rainbow effect:
     public static void intialize(){
         arduino.writeString("A");
     }
-
+//green:
     public static void green(){
         arduino.writeString("B");
     }
-
+//red:
     public static void red(){
         arduino.writeString("C");
     }
-
+//blue:
     public static void blue(){
         arduino.writeString("D");
     }
-
+//turns leds off:
     public static void off(){
         arduino.writeString("E");
     }
