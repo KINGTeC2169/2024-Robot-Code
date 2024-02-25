fb5345130ca7a234ce0d3b3be6493b339fb674e0
StrategySam <thestrategysam@gmail.com>
2024-02-25 15:19:02 -0600

    twice as fast

--- src/main/java/frc/robot/subsystems/LimelightTable.java
+++ src/main/java/frc/robot/subsystems/LimelightTable.java
@@ -86,7 +86,7 @@ public class LimelightTable {
     public static double aimShot(){
         double distance = getDistance();
         double aim = getShootingAngle(new double[]{distance, 6.90625-Vision.mountedHeight});
-        for(int i = 0; i < 15; i++){}
+        for(int i = 0; i < 8; i++){}
             aim = getShootingAngle(new double[]{distance-Arm.predictArmPosition(aim)[0], 6.90625-Vision.mountedHeight-Arm.predictArmPosition(aim)[1]});
         return aim;
     }
