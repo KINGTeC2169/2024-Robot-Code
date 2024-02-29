6b4ae93d8dcdd431dc8261f5292c98c8ec3ec8b1
StrategySam <thestrategysam@gmail.com>
2024-02-28 20:23:12 -0600

    big update

--- src/main/java/frc/robot/commands/ButtonCommands/Launch.java
+++ src/main/java/frc/robot/commands/ButtonCommands/Launch.java
@@ -3,11 +3,9 @@ package frc.robot.commands.ButtonCommands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.NoteManager;
-import frc.robot.subsystems.Shooter;
 
 public class Launch extends Command {
     private Intake intake;
-    private boolean started;
 
     public Launch(Intake intake){
         this.intake = intake;
