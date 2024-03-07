2c3ce2cb039647f9dc0a4953a48535e9d356b4d5
StrategySam <thestrategysam@gmail.com>
2024-03-06 20:55:12 -0600

    sensor pulled from previous commit

--- src/main/java/frc/robot/subsystems/NoteManager.java
+++ src/main/java/frc/robot/subsystems/NoteManager.java
@@ -1,8 +1,22 @@
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
+
+import com.revrobotics.*;
+import com.revrobotics.Rev2mDistanceSensor.Port;
+
 public class NoteManager{
     
     private static boolean note = false;
+    private static Rev2mDistanceSensor sensor = new Rev2mDistanceSensor(Port.kOnboard);
+    
+    public static void startUp(){
+        sensor.setAutomaticMode(true);
+    }
+
+    public static double getDistance(){
+        return sensor.getRange();
+    }
 
     /**This tells the shooter that there isn't a note in the intake */
     public static void setFalse(){
