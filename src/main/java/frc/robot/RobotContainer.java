896cda91ae4192af488f6b42da8cd0353163bfde
Driver Station <kingtecprogramming@gmail.com>
2024-03-14 17:32:37 -0500

    update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -130,7 +130,7 @@ public class RobotContainer {
 
     //Testing controls:
     controller.leftBumper().whileTrue(Commands.run(() -> arm.setSpeed(-controller.getRightY()/10.0)));
-    controller.rightBumper().whileTrue(new Shoot(shooter));
+    controller.rightBumper().whileTrue(new Shoot(shooter,intake));
 
     //Comp controls:
     controller.leftTrigger(0.2).onTrue(new Pickup(intake));
