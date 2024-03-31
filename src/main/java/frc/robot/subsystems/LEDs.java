bb425b1aa3cd761b6061114996c8842920b979ec
Driver Station <kingtecprogramming@gmail.com>
2024-03-30 19:05:04 -0500

    Updated gradle

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,28 +1,28 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.SerialPort;
-import frc.robot.Constants;
+//import edu.wpi.first.wpilibj.SerialPort;
+//import frc.robot.Constants;
 
 public class LEDs {   
-    private static SerialPort arduino = new SerialPort(9600, Constants.Ports.arduino);
+    //private static SerialPort arduino = new SerialPort(9600, Constants.Ports.arduino);
 //rainbow effect:
     public static void intialize(){
-        arduino.writeString("A");
+        //arduino.writeString("A");
     }
 //green:
     public static void green(){
-        arduino.writeString("B");
+        //arduino.writeString("B");
     }
 //red:
     public static void red(){
-        arduino.writeString("C");
+        //arduino.writeString("C");
     }
 //blue:
     public static void blue(){
-        arduino.writeString("D");
+        //arduino.writeString("D");
     }
 //turns leds off:
     public static void off(){
-        arduino.writeString("E");
+        //arduino.writeString("E");
     }
 }
